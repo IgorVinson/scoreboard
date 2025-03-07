5e41228a1334822bef3efd30b3c366d95901ca10
Igor T <igorvinson@gmail.com>
2025-03-07 13:34:48 -0500

    updating

--- src/components/ReportsTable.tsx
+++ src/components/ReportsTable.tsx
@@ -312,7 +312,7 @@ export function ReportsTable({
                                   </Table>
                                 </div>
                               )}
-                            </div>
+                            </div >
                           ))}
                         </div>
 
