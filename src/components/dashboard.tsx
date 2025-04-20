cda3bbab524f01e6df9dfb61094127fbee23c886
Roman Tiazhkorob <romeenice@MacBook-Air-Roman.local>
2025-04-19 21:35:10 -0700

    Оновлено ChartBlock: додано вибір метрики та динамічне оновлення графіка

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1303,7 +1303,7 @@ export function Dashboard() {
           </div>
 
         {/* Chart Block */ }
-          <ChartBlock objectives={objectives} reports={reports}/>
+          <ChartBlock reports={reports}/>
           
           
           {/* Tabs */}
